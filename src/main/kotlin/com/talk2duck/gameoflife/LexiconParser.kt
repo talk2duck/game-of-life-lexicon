de4720e88124b1a54ca1582b7b1dc1e98eb93825
albertlatacz <albert.latacz@gmail.com>
2021-01-21 15:25:17 +0000

    added parsing of lexicon-no-wrap.txt

--- src/main/kotlin/com/talk2duck/gameoflife/LexiconParser.kt
+++ src/main/kotlin/com/talk2duck/gameoflife/LexiconParser.kt
@@ -2,9 +2,15 @@ package com.talk2duck.gameoflife
 
 import com.talk2duck.gameoflife.LexiconParser.parseLexicon
 import java.net.URL
+import java.util.regex.Pattern
+import java.util.regex.Pattern.DOTALL
 
+sealed class Content
 data class Cell(val x: Int, val y: Int)
-data class Board(val populatedCells: Set<Cell> = emptySet())
+data class Text(val text: String) : Content()
+data class Board(val width: Int = 0, val height: Int = 0, val populatedCells: Set<Cell> = emptySet()) : Content()
+data class Term(val name: String, val content: List<Content> = emptyList())
+data class Lexicon(val terms: Map<String, Term>)
 
 
 /**
@@ -12,14 +18,53 @@ data class Board(val populatedCells: Set<Cell> = emptySet())
  */
 object LexiconParser {
 
-    fun parseLexicon(lexiconFile: URL) {
+    fun parseLexicon(lexiconFile: URL) : Lexicon {
         val text = lexiconFile.openStream().bufferedReader().use { it.readText() }
-        println("text = ${text}")
-
+        val split = text.split("\n\r\n").filter { it.startsWith(":") }
+        val terms = split.map(this::parseTerm).map { it.name to it }.toMap()
+        return Lexicon(terms)
     }
 
-    fun asciiToBoard(ascii: String): Board {
-        val split = ascii.split('\n').map(String::trim).filterNot(String::isEmpty)
+    private fun parseTerm(term: String): Term {
+        val split = term.split("\n")
+        val items = mutableListOf<Content>()
+        val boardItems = mutableListOf<String>()
+
+        val compile = Pattern.compile(":(.+?):", DOTALL)
+        val matcher = compile.matcher(split.first())
+
+
+        matcher.find()
+        val name = matcher.group(1)
+        val description = split.first().replace(":$name:", "").trim()
+        items.add(Text(description))
+
+        for (line in split.drop(1)) {
+
+            val charsInLine = line.trim().groupBy { it }.keys
+            if (charsInLine.all { it in setOf('.', '*', 'b', 'a') }) {
+                boardItems.add(line.trim().replace('a', '*').replace('b', '*'))
+            } else {
+                if (boardItems.isNotEmpty()) {
+                    items.add(parseBoard(boardItems.joinToString("\n") { it.trim() }))
+                    boardItems.clear()
+                }
+                items.add(Text(line.trim()))
+            }
+        }
+
+        if (boardItems.isNotEmpty()) {
+            items.add(parseBoard(boardItems.joinToString("\n") { it.trim() }))
+        }
+
+        return Term(name, items)
+    }
+
+
+
+
+    private fun parseBoard(boardAscii: String): Board {
+        val split = boardAscii.split('\n').map(String::trim).filterNot(String::isEmpty)
         val columns = split[0].length
         val rows = split.size
         val rowDelta = columns / 2
@@ -32,10 +77,10 @@ object LexiconParser {
                 if (split[y][x] == '*') cells.add(Cell(-rowDelta + x, -columnDelta + y))
             }
         }
-        return Board(cells)
+        return Board(columns, rows, cells)
     }
 }
 
 fun main() {
-    parseLexicon(LexiconParser.javaClass.getResource("/website/lexicon.txt"))
+    println(parseLexicon(LexiconParser.javaClass.getResource("/website/lexicon-no-wrap.txt")))
 }
